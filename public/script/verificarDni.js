1e81cd336238a12ffab257fddf59fedda659f6ac
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-05-08 18:51:03 -0300

    incluision del fetch en script

--- public/script/verificarDni.js
+++ public/script/verificarDni.js
@@ -1,21 +1,15 @@
 function verificarDni(dni) {
   var regexDni = /^\d{7,8}$/;
   const getdni = require("../model/getdni");
+  if (!dni) {
+    return alert("Por favor ingrese un DNI");
+  }
   if (!regexDni.test(dni)) {
     return alert("El DNI debe tener entre 7 y 8 dígitos");
   }
-  const boton = document.getElementById("button-verificar");
-  inputDni.classList.remove("light");
-
-  if (getdni(dni)) {//si el dni ya existe en la base de datos
-    boton.classList.add("success");
-    return true;
-  } else if (!getdni(dni)) {//si el dni no existe en la base de datos
-    boton.classList.add("danger");
-    return false;
-  }
-  return alert("Algo salio mal al verificar el DNI");
-
+  fetch(`/admision/verficar/${dni}`).catch((error) => {
+    console.error("Error del fetch de verificarDni:", error);
+  });
 
 }
 function turnoDni(dni) {
