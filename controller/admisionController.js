e95a223e5678d3a59c28fab6a031d4d828c2d6bb
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-05-08 18:52:09 -0300

    mejora del controlador de carga de pacientes

--- controller/admisionController.js
+++ controller/admisionController.js
@@ -1,28 +1,90 @@
+import e from "express";
+
+const Paciente = require("../models/paciente");
+const vP = require("../model/pacientesValidacion");
+
 export function controlPaciente(req, res) {
-  const { nombre, apellido, dni, fecha_nacimiento, telefono } = req.body;
-  let errores = [];
+  const paciente = { ...req.body };
+
+  let mensajeAlert = [];
   const regexName = /^[a-zA-Z\s]+$/;
   const regexDni = /^\d{7,8}$/;
-  let bandera = false;
-  if (!nombre || !apellido || !dni || !fecha_nacimiento || !telefono) {
-    return res.status(400).json({ error: "Faltan datos" });
-  }
-  if (nombre.length < 3) {
-    errores.push("El nombre debe tener al menos 3 caracteres");
-  }
-  if (apellido.length < 3) {
-    errores.push("El apellido debe tener al menos 3 caracteres");
+  const regexTelefono = /^\d{10}$/;
+  const regexEmail = /^([a-zA-Z0-9._-]+)@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  // Validar campos obligatorios
+  if (
+    !paciente.nombre ||
+    !paciente.apellido ||
+    !paciente.dni ||
+    !paciente.fecha_nacimiento ||
+    !paciente.telefono ||
+    !paciente.sexo ||
+    !paciente.obra_social ||
+    !paciente.email
+  ) {
+    return res.render("crearPaciente", { errores: "Por favor llene todos los campos obligatorios(*)" });
   }
 
-  if (!regexName.test(nombre + apellido)) {
-    errores.push("El nombre y apellido solo pueden contener letras y espacios");
+  // Validaciones especificas
+  if (paciente.nombre.length < 3) {
+    mensajeAlert.push("El nombre debe tener al menos 3 caracteres");
   }
-  if (!regexDni.test(dni)) {
-    errores.push("El DNI debe tener entre 7 y 8 dígitos");
+  if (paciente.apellido.length < 3) {
+    mensajeAlert.push("El apellido debe tener al menos 3 caracteres");
   }
-  if (bandera) {
-    return res.status(400).json({ error: errores });
+  if (!regexName.test(paciente.nombre)) {
+    mensajeAlert.push("El nombre solo puede contener letras y espacios");
   }
-  return res.status(200).json({ message: "Paciente guardado correctamente" });
+  if (!regexName.test(paciente.apellido)) {
+    mensajeAlert.push("El apellido solo puede contener letras y espacios");
+  }
+  if (!regexDni.test(paciente.dni)) {
+    mensajeAlert.push("El DNI debe tener entre 7 y 8 dígitos");
+  }
+  if (!regexTelefono.test(paciente.telefono)) {
+    mensajeAlert.push("El teléfono debe tener 10 dígitos");
+  }
+  if (paciente.telefono_contacto && !regexTelefono.test(paciente.telefono_contacto)) {
+    mensajeAlert.push("El teléfono de contacto debe tener 10 dígitos");
+  }
+  if (!regexEmail.test(paciente.email)) {
+    mensajeAlert.push("El email debe ser válido");
+  }
+  if (mensajeAlert.length > 0) {
+    return res.render("crearPaciente", { mensajeAlert, paciente });
+  }
 
+  // Crear un nuevo paciente
+  const nuevoPaciente = new Paciente(
+    paciente.dni,
+    paciente.nombre,
+    paciente.apellido,
+    paciente.fecha_nacimiento,
+    paciente.sexo,
+    paciente.telefono,
+    paciente.telefono_contacto,
+    paciente.obra_social,
+    paciente.detalle
+  );
+
+  // Guardar el paciente con model
+  console.log("Paciente guardado:", nuevoPaciente);
+  mensajeAlert.push("Paciente creado exitosamente");
+  return res.render("crearPaciente", mensajeAlert);
 }
+
+exports.crearPaciente = (req, res) => {
+  res.render("crearPaciente");
+};
+
+exports.verificarPaciente = async (req, res) => {
+  const paciente = await vP.buscarDni(req.params.dni);
+  let claseB;
+  if (!paciente) {
+    claseB = "btn-outline-danger";
+  } else {
+    claseB = "btn-outline-success";
+  }
+  return res.render("crearPaciente", { paciente, claseB });
+};
