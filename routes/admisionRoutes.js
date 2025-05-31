02d91998586e3723570629bf86ce4db43a2b2ec9
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-05-31 20:41:30 -0300

    Actualizar comentarios en admisionRoutes.js para mayor claridad en las funciones de creación y modificación de pacientes

--- routes/admisionRoutes.js
+++ routes/admisionRoutes.js
@@ -5,18 +5,18 @@ const pacienteController = require("../controllers/pacienteController");
 const infraestructuraController = require("../controllers/infraestructuraController");
 
 //+Paciente--------------------------------------
-//*form para crear un paciente
+//*form para crear un paciente/modificarlo
 router.get("/crearPaciente", pacienteController.gcrearPaciente);
 //* controla el paciente y/o lo crea/modifica
 router.post("/crearPaciente", pacienteController.controlCrearPaciente);
+//*controla para la modificacion de un paciente
+router.post("/modificarPaciente", pacienteController.modificarPaciente);
 
 //*rederiza la vista para checkear un dni
 //-se usa antes de redirrecionar:Gestion,Busqueda,Admision,Modificacion,BuscarCama
 router.get("/check", pacienteController.gcheckPaciente);
 //*checkea el dni y redirige a la vista correspondiente
 router.post("/check", pacienteController.pCheckPaciente);
-//*controla para la modificacion de un paciente
-router.post("/modificarPaciente", pacienteController.modificarPaciente);
 //*busca y muestra un paciente
 router.get("/buscarPaciente", pacienteController.busqueda);
 //*listar pacientes
