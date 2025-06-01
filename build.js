8f007014117d9d129ea3bf3b24a31ae9c03c975f
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-01 17:38:35 -0300

    Refactor build.js para mejorar la gestión del seeding y los mensajes de error al crear tablas

--- build.js
+++ build.js
@@ -1,13 +1,18 @@
 const sequelize = require("./models/db");
 require("./models/init");
-
+const seedObraSocial = require("./seeders/seedOS");
 //crear las tablas
 sequelize
   .sync({ force: true })
   .then(() => {
     console.log("Tablas creadas exitosamente");
-    process.exit();
+    return seedObraSocial(); // retornás la promesa
+  })
+  .then(() => {
+    console.log("Seed ejecutado correctamente");
+    process.exit(0);
   })
   .catch((err) => {
-    console.error("Error al crear tablas:", err);
+    console.error("Error al crear tablas o seed:", err);
+    process.exit(1);
   });
