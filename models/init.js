89a0bb6aa8cfdff0e01e2314726480e392b6540e
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-04 00:30:26 -0300

    agregacion del alias en las relaciones

--- models/init.js
+++ models/init.js
@@ -12,24 +12,29 @@ const Persona = require("./Persona");
 const Sector = require("./Sector");
 const Turno = require("./Turno");
 const MedicoEspecialidad = require("./medico_especialidad");
+
 // Relaciones Persona
 Persona.hasOne(Paciente, { foreignKey: "id_persona", as: "paciente" });
 Persona.hasOne(Medico, { foreignKey: "id_persona", as: "medico" });
 
 // Relaciones Paciente
-Paciente.belongsTo(Persona, { foreignKey: "id_persona" });
-Paciente.belongsTo(ObraSocial, { foreignKey: "id_obra_social" });
-Paciente.hasMany(Admision, { foreignKey: "id_paciente" });
-Paciente.hasMany(Turno, { foreignKey: "id_paciente" });
+Paciente.belongsTo(Persona, { foreignKey: "id_persona", as: "persona" });
+Paciente.belongsTo(ObraSocial, {
+  foreignKey: "id_obra_social",
+  as: "obraSocial",
+});
+Paciente.hasMany(Admision, { foreignKey: "id_paciente", as: "admisiones" });
+Paciente.hasMany(Turno, { foreignKey: "id_paciente", as: "turnos" });
 
 // Relaciones Medico
-Medico.belongsTo(Persona, { foreignKey: "id_persona" });
-Medico.hasMany(Turno, { foreignKey: "id_medico" });
-Medico.hasMany(Horario, { foreignKey: "id_medico" });
+Medico.belongsTo(Persona, { foreignKey: "id_persona", as: "persona" });
+Medico.hasMany(Turno, { foreignKey: "id_medico", as: "turnos" });
+Medico.hasMany(Horario, { foreignKey: "id_medico", as: "horarios" });
 Medico.belongsToMany(Especialidad, {
   through: MedicoEspecialidad,
   foreignKey: "id_medico",
   otherKey: "id_especialidad",
+  as: "especialidades",
 });
 
 // Relaciones Especialidad
@@ -37,44 +42,54 @@ Especialidad.belongsToMany(Medico, {
   through: MedicoEspecialidad,
   foreignKey: "id_especialidad",
   otherKey: "id_medico",
+  as: "medicos",
 });
 
 // Relaciones MedicoEspecialidad
-MedicoEspecialidad.belongsTo(Medico, { foreignKey: "id_medico" });
-MedicoEspecialidad.belongsTo(Especialidad, { foreignKey: "id_especialidad" });
+MedicoEspecialidad.belongsTo(Medico, { foreignKey: "id_medico", as: "medico" });
+MedicoEspecialidad.belongsTo(Especialidad, {
+  foreignKey: "id_especialidad",
+  as: "especialidad",
+});
 
 // Relaciones Turno
-Turno.belongsTo(Paciente, { foreignKey: "id_paciente" });
-Turno.belongsTo(Medico, { foreignKey: "id_medico" });
+Turno.belongsTo(Paciente, { foreignKey: "id_paciente", as: "paciente" });
+Turno.belongsTo(Medico, { foreignKey: "id_medico", as: "medico" });
 
 // Relaciones Horario
-Horario.belongsTo(Medico, { foreignKey: "id_medico" });
+Horario.belongsTo(Medico, { foreignKey: "id_medico", as: "medico" });
 
 // Relaciones Admision
-Admision.belongsTo(Paciente, { foreignKey: "id_paciente" });
-Admision.belongsTo(Motivos, { foreignKey: "id_motivo" });
-Admision.hasMany(MovimientoCama, { foreignKey: "id_admision" });
+Admision.belongsTo(Paciente, { foreignKey: "id_paciente", as: "paciente" });
+Admision.belongsTo(Motivos, { foreignKey: "id_motivo", as: "motivo" });
+Admision.hasMany(MovimientoCama, {
+  foreignKey: "id_admision",
+  as: "movimientosCama",
+});
 
 // Relaciones Motivos
-Motivos.hasMany(Admision, { foreignKey: "id_motivo" });
+Motivos.hasMany(Admision, { foreignKey: "id_motivo", as: "admisiones" });
 
 // Relaciones ObraSocial
-ObraSocial.hasMany(Paciente, { foreignKey: "id_obra_social" });
+ObraSocial.hasMany(Paciente, { foreignKey: "id_obra_social", as: "pacientes" });
 
 // Relaciones MovimientoCama
-MovimientoCama.belongsTo(Admision, { foreignKey: "id_admision" });
-MovimientoCama.belongsTo(Cama, { foreignKey: "id_cama" });
+MovimientoCama.belongsTo(Admision, {
+  foreignKey: "id_admision",
+  as: "admision",
+});
+MovimientoCama.belongsTo(Cama, { foreignKey: "id_cama", as: "cama" });
 
 // Relaciones Cama
-Cama.belongsTo(Habitacion, { foreignKey: "id_habitacion" });
-Cama.hasMany(MovimientoCama, { foreignKey: "id_cama" });
+Cama.belongsTo(Habitacion, { foreignKey: "id_habitacion", as: "habitacion" });
+Cama.hasMany(MovimientoCama, { foreignKey: "id_cama", as: "movimientosCama" });
 
 // Relaciones Habitacion
-Habitacion.belongsTo(Sector, { foreignKey: "id_sector" });
-Habitacion.hasMany(Cama, { foreignKey: "id_habitacion" });
+Habitacion.belongsTo(Sector, { foreignKey: "id_sector", as: "sector" });
+Habitacion.hasMany(Cama, { foreignKey: "id_habitacion", as: "camas" });
 
 // Relaciones Sector
-Sector.hasMany(Habitacion, { foreignKey: "id_sector" });
+Sector.hasMany(Habitacion, { foreignKey: "id_sector", as: "habitaciones" });
 
 module.exports = {
   Admision,
