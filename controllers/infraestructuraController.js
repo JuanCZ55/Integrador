85531a0e8690c3dfc1697a6d247db969f0a848d0
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-05 01:29:43 -0300

    Se agregaron endpoints en la API para consultar habitaciones y camas disponibles.

--- controllers/infraestructuraController.js
+++ controllers/infraestructuraController.js
@@ -1,15 +1,75 @@
 const {
-  Paciente,
-  ObraSocial,
-  Persona,
-  Motivos,
+  Cama,
+  Habitacion,
+  Sector,
+  MovimientoCama,
   Admision,
-  Turno,
+  Paciente,
+  Persona,
 } = require("../models/init");
 const sequelize = require("../models/db");
 const { Op } = require("sequelize");
 
 async function crearCama() {}
+// GET /api/apiHabitacionesLibres?id_sector=2&genero=m
+async function apiHabitacionesLibres(req, res) {
+  const { id_sector, genero } = req.query;
+
+  try {
+    // Configuración de consulta optimizada
+    const habitaciones = await Habitacion.findAll({
+      where: {
+        id_sector: id_sector,
+        [Op.or]: [
+          { genero: null }, // Habitaciones sin nadie
+          { genero: genero }, // fitro genero
+        ],
+      },
+      include: [
+        {
+          model: Cama,
+          as: "camas",
+          where: { estado: 1 },
+          attributes: ["id_cama"], // Solo IDs para reducir datos
+        },
+        {
+          model: Sector,
+          as: "sector",
+          attributes: ["nombre"],
+        },
+      ],
+    });
+
+    return res.status(200).json(habitaciones);
+  } catch (error) {
+    console.error("Error al buscar habitaciones libres:", error);
+    return res.status(500).json({ error: "Error interno del servidor" });
+  }
+}
+
+// GET /api/apiCamasLibresxHabitacion?id_habitacion=X
+async function apiCamasLibres(req, res) {
+  const { id_habitacion } = req.query;
+
+  try {
+    // Traemos todas las camas de la habitacion con su movimiento actual
+    const camas = await Cama.findAll({
+      where: {
+        id_habitacion: id_habitacion,
+        estado: 1,
+      },
+    });
+
+    return res.status(200).json(camas);
+  } catch (error) {
+    console.error("Error al cargar camas libres:", error);
+    return res.status(500).json({ error: "Error al cargar camas libres" });
+  }
+}
+
 module.exports = {
   crearCama,
+
+  apiHabitacionesLibres,
+  apiCamasLibres,
 };
