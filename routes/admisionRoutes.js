787ad97795a634181b92e184a600840d8ace5417
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-08 20:39:19 -0300

    Refactor rutas de pacientes en admisionRoutes.js; actualizar controladores y eliminar código comentado innecesario.

--- routes/admisionRoutes.js
+++ routes/admisionRoutes.js
@@ -5,13 +5,6 @@ const pacienteController = require("../controllers/pacienteController");
 const infraestructuraController = require("../controllers/infraestructuraController");
 const pacienteC = require("../controllers/pacienteC");
 
-// //+Paciente--------------------------------------
-// //*form para crear un paciente/modificarlo
-// router.get("/crearPaciente", pacienteController.gcrearPaciente);
-// //* controla el paciente y/o lo crea/modifica
-// router.post("/crearPaciente", pacienteController.controlCrearPaciente);
-// //*controla para la modificacion de un paciente
-// router.post("/modificarPaciente", pacienteController.modificarPaciente);
 //+PacienteNuevo--------------------------------------
 //*form para crear un paciente/modificarlo
 router.get("/crearPaciente", pacienteC.gCrearPaciente);
@@ -19,16 +12,10 @@ router.get("/crearPaciente", pacienteC.gCrearPaciente);
 router.post("/crearPaciente", pacienteC.pCrearPaciente);
 //*controla para la modificacion de un paciente
 router.post("/modificarPaciente", pacienteC.pModificarPaciente);
-
-//*rederiza la vista para checkear un dni
-//-se usa antes de redirrecionar:Gestion,Busqueda,Admision,Modificacion,BuscarCama
-router.get("/check", pacienteController.gcheckPaciente);
-//*checkea el dni y redirige a la vista correspondiente
-router.post("/check", pacienteController.pCheckPaciente);
-//*busca y muestra un paciente
-router.get("/buscarPaciente", pacienteController.busqueda);
 //*listar pacientes
-router.get("/listarPacientes", pacienteController.listarPacientes);
+router.get("/listarPacientes", pacienteC.listarPacientes);
+
+//*api de busqueda
 router.get("/api/busqueda", pacienteController.busquedaApi);
 
 //+Admision----------------------------------------
@@ -41,13 +28,29 @@ router.get("", admisionController.inicio);
 router.get("/gestionarAdmision", admisionController.admision);
 //**crea la admision */
 router.post("/gestionarAdmision", admisionController.pAdmision);
+//*Lista de admisiones
+router.get("/listaAdmision", admisionController.listaAdmisiones);
+//*Cancelar admision
+router.post("/cancelarAdmision", admisionController.cancelarAdmision);
+//*cambiar paciente de admision
+router.get(
+  "/cambiarPacienteAdmisiones",
+  admisionController.cambiarPacienteAdmisiones
+);
+router.post(
+  "/cambiarPacienteAdmisiones",
+  admisionController.cambiarPacienteAdmisiones
+);
+//*Api de busqeda
 router.get("/api/busqueda", pacienteController.busqueda);
 
 //+Infrestructura--------------------------------------
+router.get("/listaCamas", infraestructuraController.listaCamas);
 router.get(
   "/api/habitaciones",
   infraestructuraController.apiHabitacionesLibres
 );
+
 router.get("/api/camas", infraestructuraController.apiCamasLibres);
 
 module.exports = router;
