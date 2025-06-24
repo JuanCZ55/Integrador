d839c6dc4a75d7abc7543431d29c37fe309fd47d
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-24 16:51:10 -0300

    Actualizar configuración de Sequelize para usar PostgreSQL y agregar dependencia de pg

--- models/db.js
+++ models/db.js
@@ -1,16 +1,24 @@
-const { Sequelize } = require("sequelize");
+// db.js
 require("dotenv").config();
+const pg = require("pg");
+const { Sequelize } = require("sequelize");
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
-    dialectModule: require("mysql2"),
+    dialect: "postgres",
+    dialectModule: pg,
     timezone: "-03:00",
     logging: false,
+
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false },
+      preferQueryMode: "simple",
+    },
   }
 );
 
