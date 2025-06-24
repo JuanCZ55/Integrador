e433c981606480b59215a84d660bb4325e0d4537
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-24 16:50:44 -0300

    Actualizar estado de motivos y personas a booleano en seeders

--- seeders/seedMotivos.js
+++ seeders/seedMotivos.js
@@ -3,30 +3,26 @@ const sequelize = require("../models/db");
 async function seedMotivos() {
   const queryInterface = sequelize.getQueryInterface();
 
-  await queryInterface.bulkInsert(
-    "motivos",
-    [
-      {
-        nombre: "Emergencia",
-        estado: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        nombre: "Derivado",
-        estado: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        nombre: "Turno",
-        estado: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ],
-    {}
-  );
+  await queryInterface.bulkInsert("motivos", [
+    {
+      nombre: "Emergencia",
+      estado: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      nombre: "Derivado",
+      estado: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      nombre: "Turno",
+      estado: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]);
 }
 
 module.exports = seedMotivos;
