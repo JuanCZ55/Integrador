243625465691870fa8f8155b134d84f82be52e14
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-07-11 19:28:29 -0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 router.get("/inicio", (req, res) => {
-  res.render("admin/inicio");
+  res.render("admin/medico");
 });
 
 router.get("/usuarios", (req, res) => {
