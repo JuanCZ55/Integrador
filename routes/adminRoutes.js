4938bda9e2c9a3f9815fa2ddc4a569be369e4768
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-07-12 19:10:10 -0300

    Se corrigió la estructura de las rutas en adminRoutes.js para asegurar que todas las vistas se rendericen correctamente.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 router.get("/inicio", (req, res) => {
+  res.render("admin/inicio");
+});
+router.get("/empleado", (req, res) => {
+  res.render("admin/empleado");
+});
+router.get("/medico", (req, res) => {
   res.render("admin/medico");
+});
+router.get("/horariosEmpleado", (req, res) => {
+  res.render("admin/horariosEmpleado");
 });
 
 router.get("/usuarios", (req, res) => {
