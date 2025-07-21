5595d8f2fa99b8e115ddcd9593ebc6a225f562f1
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-07-21 20:28:53 -0300

    fix(seed): se corrigió asignación de id_persona en seeders para que se agreguen automaticamente en la bd

--- seeders/seedRU.js
+++ seeders/seedRU.js
@@ -15,7 +15,6 @@ async function seedRolesUsuarios() {
   // 2. Crear personas para los usuarios
   const personas = [
     {
-      id_persona: 16,
       dni: 10000001,
       nombre: "Admin",
       apellido: "Sistema",
@@ -23,7 +22,6 @@ async function seedRolesUsuarios() {
       genero: "Otro",
     },
     {
-      id_persona: 17,
       dni: 10000002,
       nombre: "Admi",
       apellido: "Admisiones",
@@ -31,7 +29,6 @@ async function seedRolesUsuarios() {
       genero: "Otro",
     },
     {
-      id_persona: 18,
       dni: 10000003,
       nombre: "Medi",
       apellido: "Medico",
@@ -39,7 +36,6 @@ async function seedRolesUsuarios() {
       genero: "Otro",
     },
     {
-      id_persona: 19,
       dni: 10000004,
       nombre: "Enfe",
       apellido: "Enfermeria",
@@ -59,7 +55,7 @@ async function seedRolesUsuarios() {
   // 3. Crear empleados para cada persona y rol
   const empleados = [
     {
-      id_persona: personasCreadas[0].id_persona,
+      id_persona: 16,
       id_rol: 1,
       fecha_ingreso: "2020-01-01",
       estado: 1,
@@ -67,7 +63,7 @@ async function seedRolesUsuarios() {
       updatedAt: new Date(),
     },
     {
-      id_persona: personasCreadas[1].id_persona,
+      id_persona: 17,
       id_rol: 2,
       fecha_ingreso: "2020-01-01",
       estado: 1,
@@ -75,7 +71,7 @@ async function seedRolesUsuarios() {
       updatedAt: new Date(),
     },
     {
-      id_persona: personasCreadas[2].id_persona,
+      id_persona: 18,
       id_rol: 3,
       fecha_ingreso: "2020-01-01",
       estado: 1,
@@ -83,7 +79,7 @@ async function seedRolesUsuarios() {
       updatedAt: new Date(),
     },
     {
-      id_persona: personasCreadas[3].id_persona,
+      id_persona: 19,
       id_rol: 4,
       fecha_ingreso: "2020-01-01",
       estado: 1,
