2d7aef7a1ef93816fcf71b70c60e35e8654c98f4
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-07-30 17:33:58 -0300

    Se añadió ruta para empleados de AA

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const MEController = require("../controllers/admin/MEController");
+const empleadoController = require("../controllers/admin/empleadoController");
 const router = express.Router();
 router.get("/inicio", (req, res) => {
   res.render("admin/inicio");
 });
-router.get("/empleado", (req, res) => {
-  res.render("admin/empleado");
-});
+//ruta para empleado de AA(Adminstacion-Admision)
+router.get("/empleado", empleadoController.getAA);
+
 //ruta para ME(Medico-Enfermero)
 router.get("/medico", MEController.getME);
 router.post("/medico", MEController.postME);
