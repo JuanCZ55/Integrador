38630ea3adb556ae82db335451a9e58af7169845
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-11-27 22:41:42 -0300

    Agregó controladores para historial médico y medicación, y eliminó el controlador de enfermería.

---
+++ controllers/enfermeria/historiaController.js
@@ -0,0 +1,10 @@
+const { HistorialMedico } = require("../../models/init");
+
+async function getHistoria(req, res) {}
+
+async function postHistoria(req, res) {}
+
+module.exports = {
+  getHistoria,
+  postHistoria,
+};
---
+++ controllers/enfermeria/medicacionController.js
@@ -0,0 +1,10 @@
+const { AdministracionMedicamentos } = require("../../models/init");
+
+async function getMedicacion(req, res) {}
+
+async function postMedicacion(req, res) {}
+
+module.exports = {
+  renderMedicacion,
+  postMedicacion,
+};
