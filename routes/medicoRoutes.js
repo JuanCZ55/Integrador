730cbad5337ca0a2c047cb0cf4f96b080546f93f
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-12-01 19:59:27 -0300

    Agrega rutas para gestionar pacientes sin médico asignado y funcionalidades de asignación

--- routes/medicoRoutes.js
+++ routes/medicoRoutes.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+  getSinAsignar,
+  postAsignacion,
+  getMedicosByEspecialidad,
+  buscarPaciente,
+} = require("../controllers/medico/sinAsingarController");
+const { getMisPacientes } = require("../controllers/medico/misPacientes");
+
+router.get("/sinAsignar", getSinAsignar);
+router.post("/asignarPaciente", postAsignacion);
+router.get("/api/medicos", getMedicosByEspecialidad);
+router.get("/api/buscarPaciente", buscarPaciente);
+
+router.get("/misPacientes", getMisPacientes);
+
 router.get("/inicio", (req, res) => {
   res.render("medico/inicio");
 });
